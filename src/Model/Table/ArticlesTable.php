222cc39e1754b4c1e011294d4c96f199ae6f83e2
req.k.akiyama <req.k.akiyama@gmail.com>
2020-03-26 05:26:41 +0000

    記事テーブルにValidationの定義を追加

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -5,6 +5,7 @@ namespace App\Model\Table;
 
 use Cake\ORM\Table;
 use Cake\Utility\Text;
+use Cake\Validation\Validator;
 
 /**
  * 記事テーブルを表現するORMクラス
@@ -35,5 +36,24 @@ class ArticlesTable extends Table
             $sluggedTitle = Text::slug($entity->title);
             $entity->slug = substr($sluggedTitle, 0, 191);
         }
+    }
+
+    /**
+     * 既定のvalidationロジックを定義する
+     *
+     * @param Validator $validator Validatorインスタンス
+     * @return Validator 設定後のvalidator
+     */
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->notEmptyString('title')
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+
+            ->notEmptyString('body')
+            ->minLength('body', 10);
+
+        return $validator;
     }
 }
