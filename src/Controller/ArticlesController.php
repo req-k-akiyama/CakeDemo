66de7f8bf73982bab049a3a4ef01030c196a8631
req.k.akiyama <req.k.akiyama@gmail.com>
2020-03-26 05:02:17 +0000

    記事を追加する機能を実装

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -9,6 +9,16 @@ namespace App\Controller;
 class ArticlesController extends AppController
 {
     /**
+     * ArticlesController を初期化する
+     */
+    public function initialize(): void
+    {
+        parent::initialize();
+        $this->loadComponent('Paginator');
+        $this->loadComponent('Flash');
+    }
+
+    /**
      * GET /articles/index
      */
     public function index(): void
@@ -27,5 +37,28 @@ class ArticlesController extends AppController
     {
         $article = $this->Articles->findBySlug($slug)->firstOrFail();
         $this->set(compact('article'));
+    }
+
+    /**
+     * GET,POST /articles/add
+     */
+    public function add(): ?\Cake\Http\Response
+    {
+        $article = $this->Articles->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+
+            // TODO: Hardcoding the user_id is temporary, and will be removed later
+            // when we build authentication out.
+            $article->user_id = 1;
+
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your article.'));
+        }
+        $this->set('article', $article);
+        return null;
     }
 }
--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Utility\Text;
 
 /**
  * 記事テーブルを表現するORMクラス
@@ -18,5 +19,21 @@ class ArticlesTable extends Table
     public function initialize(array $config): void
     {
         $this->addBehavior('Timestamp');
+    }
+
+    /**
+     * 永続化前の処理を実行する
+     *
+     * @param \Cake\Event\EventInterface $event イベント情報
+     * @param \App\Model\Entity\Article $entity 作成するエンティティ
+     * @param \ArrayObject $options オプション
+     */
+    public function beforeSave($event, $entity, $options): void
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            // 記事タイトルからslugを生成する
+            $sluggedTitle = Text::slug($entity->title);
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
     }
 }
